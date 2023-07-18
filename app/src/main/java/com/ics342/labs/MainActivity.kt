22761c0ec6e8310be715f668f70f7416fd2c6805
6urk <82283209+6urk@users.noreply.github.com>
2023-07-17 23:05:26 -0500

    lab6

--- app/src/main/java/com/ics342/labs/MainActivity.kt
+++ app/src/main/java/com/ics342/labs/MainActivity.kt
@@ -5,16 +5,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.ics342.labs.ui.theme.LabsTheme
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
-import com.squareup.moshi.adapter
+import com.squareup.moshi.Types
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 
 class MainActivity : ComponentActivity() {
@@ -25,26 +22,22 @@ class MainActivity : ComponentActivity() {
         setContent {
             LabsTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    /*
-                    Display the items from the Json file in a LazyColumn
-                     */
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    // Display the items from the JSON file in a LazyColumn
+                    ForecastScreen(data)
                 }
             }
         }
     }
-}
+    private fun loadData(resources: Resources): String {
+        return resources.openRawResource(R.raw.data).bufferedReader().use { it.readText() }
+    }
 
-private fun loadData(resources: Resources): String {
-    return resources
-        .openRawResource(R.raw.data)
-        .bufferedReader()
-        .use { it.readText() }
-}
+    private fun dataFromJsonString(json: String): List<Person> {
+        val moshi: Moshi = Moshi.Builder().addLast(KotlinJsonAdapterFactory()).build()
+        val listType = Types.newParameterizedType(List::class.java, Person::class.java)
+        val adapter: JsonAdapter<List<Person>> = moshi.adapter(listType)
+        return adapter.fromJson(json) ?: emptyList()
+    }
 
-@OptIn(ExperimentalStdlibApi::class)
-private fun dataFromJsonString(json: String): List</* put your data class name here */> {
-    val moshi: Moshi = Moshi.Builder().addLast(KotlinJsonAdapterFactory()).build()
-    val jsonAdapter: JsonAdapter<List</* put your data class name here */>> = moshi.adapter()
-    return jsonAdapter.fromJson(json) ?: listOf()
 }
