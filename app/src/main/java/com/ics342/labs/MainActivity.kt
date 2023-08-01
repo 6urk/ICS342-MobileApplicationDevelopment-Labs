6f54f4966cb1bd8f3d078e79209ef6255c87887c
6urk <82283209+6urk@users.noreply.github.com>
2023-07-31 23:25:51 -0500

    My Lab 8 changes

--- app/src/main/java/com/ics342/labs/MainActivity.kt
+++ app/src/main/java/com/ics342/labs/MainActivity.kt
@@ -2,7 +2,6 @@ package com.ics342.labs
 
 import android.Manifest
 import android.content.Context
-import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -12,6 +11,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -22,10 +22,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.content.ContextCompat
 import com.ics342.labs.ui.theme.LabsTheme
 
@@ -34,25 +32,41 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             var hasPermission by remember { mutableStateOf(false) }
+            var showPermissionRationale by remember { mutableStateOf(false) }
             val context = LocalContext.current
-            val launcher = rememberLauncherForActivityResult(RequestPermission()) {
-                if (it) {
-                    startNotificationService(context)
+
+            val launcher = rememberLauncherForActivityResult(
+                RequestPermission()
+            ) { isGranted ->
+                if (isGranted) {
+                    hasPermission = true
+                } else {
+                    showPermissionRationale = shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)
                 }
             }
+
             LabsTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                        Button(
-                            onClick = {
+                        if (hasPermission) {
+                            LocationView()
+                        } else {
+                            Button(onClick = {
                                 checkOrRequestPermission(
                                     context,
+                                    Manifest.permission.POST_NOTIFICATIONS,
                                     launcher
-                                ) { startNotificationService(context) }
+                                ) { hasPermission = true }
+                            }) {
+                                Text("Request Notification Permission")
                             }
-                        ) {
-                            Text("Show Notification")
+                        }
+
+                        if (showPermissionRationale) {
+                            PermissionRationaleDialog(
+                                onConfirm = { launcher.launch(Manifest.permission.POST_NOTIFICATIONS) },
+                                onCancel = { showPermissionRationale = false }
+                            )
                         }
                     }
                 }
@@ -61,22 +75,49 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-fun startNotificationService(context: Context) {
-    TODO("Start the Notification Service")
+@Composable
+fun LocationView() {
+    Text("Has Notification Permission")
+}
+
+@Composable
+fun PermissionRationaleDialog(
+    onConfirm: () -> Unit,
+    onCancel: () -> Unit
+) {
+    AlertDialog(
+        onDismissRequest = {},
+        confirmButton = {
+            Button(onClick = { onConfirm() }) {
+                Text("OK")
+            }
+        },
+        dismissButton = {
+            Button(onClick = { onCancel() }) {
+                Text("No")
+            }
+        },
+        title = {
+            Text("Permissions Required")
+        },
+        text = {
+            Text("This app requires notification permission to show notifications.")
+        }
+    )
 }
 
 private fun checkOrRequestPermission(
     context: Context,
+    permission: String,
     launcher: ManagedActivityResultLauncher<String, Boolean>,
     permissionGranted: () -> Unit
 ) {
-    if (
-        ContextCompat.checkSelfPermission(
-            context,
-            Manifest.permission.POST_NOTIFICATIONS
-        ) == PackageManager.PERMISSION_GRANTED) {
-        permissionGranted()
-    } else {
-        launcher.launch(Manifest.permission.POST_NOTIFICATIONS)
+    when (PackageManager.PERMISSION_GRANTED) {
+        ContextCompat.checkSelfPermission(context, permission) -> {
+            permissionGranted()
+        }
+        else -> {
+            launcher.launch(permission)
+        }
     }
 }
