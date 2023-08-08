0033d078b8b6cb21bfb342dea681279e08f48c6f
6urk <82283209+6urk@users.noreply.github.com>
2023-08-07 23:41:45 -0500

    lab9

--- app/src/main/java/com/ics342/labs/MainActivity.kt
+++ app/src/main/java/com/ics342/labs/MainActivity.kt
@@ -1,87 +1,39 @@
 package com.ics342.labs
-
-import ForecastViewModel
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import coil.compose.AsyncImage
+import androidx.compose.ui.tooling.preview.Preview
 import com.ics342.labs.ui.theme.LabsTheme
-import dagger.hilt.android.AndroidEntryPoint
-
-@MainActivity.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    annotation class AndroidEntryPoint
-
-    private val currentConditionsViewModel: CurrentConditionsViewModel by viewModels()
-    private val forecastViewModel: ForecastViewModel by viewModels()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             LabsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Column {
-                        CurrentConditionsView(currentConditionsViewModel)
-                        ForecastView(forecastViewModel)
-                    }
+                    Greeting("Android")
                 }
             }
         }
     }
 }
-
 @Composable
-fun CurrentConditionsView(
-    viewModel: CurrentConditionsViewModel
-) {
-    val currentWeather = viewModel.currentWeather.collectAsState(null)
-
-    LaunchedEffect(Unit) {
-        viewModel.fetchCurrentWeather("55407", "+1", "a2d3351eaf14e522f80fe5130d49e421")
-    }
-
-    currentWeather.value?.let { weather ->
-        Column {
-            Text(text = "High Temp: ${weather.highTemp}")
-            WeatherConditionIcon(url = weather.iconUrl)
-        }
-    }
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
 }
-
+@Preview(showBackground = true)
 @Composable
-fun ForecastView(
-    viewModel: ForecastViewModel
-) {
-    val forecast = viewModel.forecast.collectAsState(null)
-
-    LaunchedEffect(Unit) {
-        viewModel.fetchForecast("55407", "+1", "a2d3351eaf14e522f80fe5130d49e421")
+fun GreetingPreview() {
+    LabsTheme {
+        Greeting("Android")
     }
-
-    forecast.value?.let { forecastData ->
-        Column {
-            Text("Forecast")
-            forecastData.forecasts.forEach { forecastItem ->
-                WeatherConditionIcon(url = forecastItem.iconUrl)
-            }
-        }
-    }
-}
-
-@Composable
-fun WeatherConditionIcon(
-    url: String
-) {
-    AsyncImage(model = url, contentDescription = "")
 }
